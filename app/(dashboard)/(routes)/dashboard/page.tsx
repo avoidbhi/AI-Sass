21ece9e37a7c053ea9e03c07956477d9530b8098
Abhijeet Kanase <chillgoziecoder@outlook.com>
2023-07-29 20:38:08 +0530

    initial commit

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        {/* below is the inscription from Shad-CN UI */}
+        <p className="text-muted-foreground text-sm md:text-lg text-center">
+          Chat with he smartest AI - Experience the power of AI
+        </p>
+      </div>
     </div>
   );
 };
