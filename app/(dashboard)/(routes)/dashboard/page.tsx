7646b11b80bfa7a7c5e517990043c8e689eebeef
Abhijeet Kanase <chillgoziecoder@outlook.com>
2023-07-30 23:18:23 +0530

    initial commit

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  }
+]
 
 const DashboardPage = () => {
   return (
@@ -10,6 +24,16 @@ const DashboardPage = () => {
         <p className="text-muted-foreground text-sm md:text-lg text-center">
           Chat with he smartest AI - Experience the power of AI
         </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          {tools.map((tool)=> (
+            <Card
+                key={tool.href}
+                className="p-4 border-black/5 flex items-center justify-between hover:shadow-md
+                            transition cursor-pointer">
+
+            </Card>
+          ))}
       </div>
     </div>
   );
